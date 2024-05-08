d17312298ef001143f9409d80615ff033d16250f
1234Bruno1234 <138172731+1234Bruno1234@users.noreply.github.com>
2024-05-07 22:16:49 -0300

    fijate que onda

--- js/javascript.js
+++ js/javascript.js
@@ -15,22 +15,45 @@ function reparteTarjetas() {
   var mesa = document.querySelector("#mesa");
   var tarjetasBarajadas = barajaTarjetas();
   mesa.innerHTML = "";
+  let i = 1;
 
   tarjetasBarajadas.forEach(function(elemento) {
     var tarjeta = document.createElement("div");
 
     tarjeta.innerHTML =
-      "<div class='tarjeta'>" +
+      "<div class='tarjeta' id='"+ i +"'>" +
       "<div class='tarjeta__contenido'>" +
       elemento +
       "</div>" +
       "</div>";
 
     mesa.appendChild(tarjeta);
+    i += 1;
   });
 }
 
-function descubrir() {
+function descubrir(e) {
+  const id = e.target.id;
+  let elegidos = [0,0];
+  if (elegidos[0] == 0) { 
+    elegidos[0] = id;
+  } else {
+    elegidos[1] = id;
+  }
+
+  let emojis = ["",""];
+  if (emojis[0] == 0) { 
+    emojis[0] = e.target.innerText;
+  } else {
+    emojis[1] = e.target.innerText;
+  }
+
+  if (emojis[0] == emojis[1]) {
+    console.log("coinciden");
+  }
+  //verificar si ya se dieron todas las coincidencias
+  //longitud * 2 = contador
+
   this.classList.add("descubierta");
 }
 
